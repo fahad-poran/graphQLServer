bbc5d1f1a88e0d1c37dec017fa1c7b1220f5ac91
Abdullah Al Fahad <abdullahalfahad@Abdullahs-MacBook-Air.local>
2022-06-17 01:53:52 +0600

    heroku

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ const connectDB = require('./config/db');
 const cors = require('cors');
 const express = require('express');
 const schema = require('./schema/schema');
-
+ 
 const port = process.env.PORT || 5000;
 const {graphqlHTTP} = require('express-graphql');
+const { application } = require('express');
 const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+        origin: "*",
+        credentials: true,
+}));
         //end point
 app.use('/graphql',graphqlHTTP({
      schema,
      graphiql: process.env.NODE_ENV === 'development'
 }))
 
-
+app.get('/',(req,res)=>{
+        res.send('Running Server For Heroku');
+});
 app.listen(port,console.log(`server running on port: ${port}`));
