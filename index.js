52db66303e970176c8ea581349ce417cb33aa408
Abdullah Al Fahad <abdullahalfahad@Abdullahs-MacBook-Air.local>
2022-06-22 21:41:11 +0600

    changer

--- index.js
+++ index.js
@@ -12,18 +12,7 @@ const app = express();
 
 connectDB();
 
-// Serve frontend
-if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/build')));
-      
-        app.get('*', (req, res) =>
-          res.sendFile(
-            path.resolve(__dirname, '../', 'client', 'build', 'index.html')
-          )
-        );
-      } else {
-        app.get('/', (req, res) => res.send('Please set to production'));
-      }
+
 
 app.use(cors({
         origin: "*",
@@ -38,4 +27,17 @@ app.use('/graphql',graphqlHTTP({
 app.get('/',(req,res)=>{
         res.send('Running Server For Heroku');
 });
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/build')));
+      
+        app.get('*', (req, res) =>
+          res.sendFile(
+            path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+          )
+        );
+      } else {
+        app.get('/', (req, res) => res.send('Please set to production'));
+      }
 app.listen(port,console.log(`server running on port: ${port}`));
