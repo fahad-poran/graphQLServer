79ed0df3156768ab4b14f023640dc66438ce927b
Abdullah Al Fahad <abdullahalfahad@Abdullahs-MacBook-Air.local>
2022-06-22 21:22:26 +0600

    some chnages

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const cors = require('cors');
@@ -11,8 +12,21 @@ const app = express();
 
 connectDB();
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/build')));
+      
+        app.get('*', (req, res) =>
+          res.sendFile(
+            path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+          )
+        );
+      } else {
+        app.get('/', (req, res) => res.send('Please set to production'));
+      }
+
 app.use(cors({
-        origin: "https://radiant-dusk-79746.herokuapp.com/",
+        origin: "*",
         credentials: true,
 }));
         //end point
